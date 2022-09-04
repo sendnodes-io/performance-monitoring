bac0b2ae3b97add9121b80e6fc97abc01e0d687b
BigBoss <bigboss@sendnodes.io>
2022-09-04 14:35:10 -0500

    fix formatting for mobile

--- core/discord.py
+++ core/discord.py
@@ -14,7 +14,7 @@ class DiscordBot():
         env_var = utils.get_env_variables([cst.DISCORD_WEBHOOK])
         self.discord_hook = env_var.get(cst.DISCORD_WEBHOOK)
         self.webhook = DiscordWebhook(url=env_var.get(
-            cst.DISCORD_WEBHOOK), username='Perf Bot')
+            cst.DISCORD_WEBHOOK), username='Perf Bot',  rate_limit_retry=True)
 
     def post_network_perf_data(self, network_data: NetworkPerformance):
         description = f'Today Pokt: {network_data.today_pokt}\n30d avg Pokt: {network_data.thirty_day_pokt_avg}'
@@ -32,72 +32,78 @@ class DiscordBot():
         runners_data.sort(key=lambda r: (r.avg_last_48_hours,
                           r.runner_domain_sort), reverse=True)
         description = tabulate(
-            [[r.runner_domain, f'{round(r.avg_last_48_hours, 3)}'] for r in runners_data], ["domain", "Average 48h"], tablefmt="pretty",  showindex=range(1, len(runners_data) + 1))
+            [[r.runner_domain, f'{round(r.avg_last_48_hours, 3)}'] for r in runners_data], ["domain", "Average 48h"], maxcolwidths=[22, 12])
         embed_48 = DiscordEmbed(
             title=f"Average 48h", description=f'```{description}```', color='03b2f8')
         self.webhook.add_embed(embed_48)
+        # self.webhook.content = f'```{description}```'
         self.webhook.execute()
-        time.sleep(2)
+        # time.sleep(2)
         self.webhook.remove_embeds()
 
         # Last 24 hours
         runners_data.sort(key=lambda r: (r.avg_last_24_hours,
                           r.runner_domain_sort), reverse=True)
         description = tabulate(
-            [[r.runner_domain, f'{round(r.avg_last_24_hours, 3)}'] for r in runners_data], ["domain", "Average 24h"], tablefmt="pretty",  showindex=range(1, len(runners_data) + 1))
+            [[r.runner_domain, f'{round(r.avg_last_24_hours, 3)}'] for r in runners_data], ["domain", "Average 24h"], maxcolwidths=[22, 12])
         embed_24 = DiscordEmbed(
             title=f"Average 24h", description=f'```{description}```', color='66ff66')
         self.webhook.add_embed(embed_24)
+        # self.webhook.content = f'```{description}```'
         self.webhook.execute()
-        time.sleep(2)
+        # time.sleep(2)
         self.webhook.remove_embeds()
 
         # Last 6 hours
         runners_data.sort(key=lambda r: (r.avg_last_6_hours,
                           r.runner_domain_sort), reverse=True)
         description = tabulate(
-            [[r.runner_domain, f'{round(r.avg_last_6_hours, 3)}'] for r in runners_data], ["domain", "Average 6h"], tablefmt="pretty",  showindex=range(1, len(runners_data) + 1))
+            [[r.runner_domain, f'{round(r.avg_last_6_hours, 3)}'] for r in runners_data], ["domain", "Average 6h"], maxcolwidths=[22, 12])
         embed_6 = DiscordEmbed(
             title=f"Average 6h", description=f'```{description}```', color='7d7d73')
         self.webhook.add_embed(embed_6)
+        # self.webhook.content = f'```{description}```'
         self.webhook.execute()
-        time.sleep(2)
+        # time.sleep(2)
         self.webhook.remove_embeds()
 
         # Chains count
         runners_data.sort(key=lambda r: (
             r.total_chains, r.runner_domain_sort), reverse=True)
         description = tabulate(
-            [[r.runner_domain, f'{r.total_chains:,}'] for r in runners_data], ["domain", "# chains"], tablefmt="pretty",  showindex=range(1, len(runners_data) + 1), colalign=("left", "left", "right"))
+            [[r.runner_domain, f'{r.total_chains:,}'] for r in runners_data], ["domain", "# chains"], colalign=("left", "right"), maxcolwidths=[22, 12])
         embed_c = DiscordEmbed(
             title=f"# chains", description=f'```{description}```', color='ffff00')
         self.webhook.add_embed(embed_c)
+        # self.webhook.content = f'```{description}```'
         self.webhook.execute()
-        time.sleep(2)
+        # time.sleep(2)
         self.webhook.remove_embeds()
 
         # Nodes count
         runners_data.sort(key=lambda r: (
             r.total_nodes, r.runner_domain_sort), reverse=True)
         description = tabulate(
-            [[r.runner_domain, f'{r.total_nodes:,}'] for r in runners_data], ["domain", "# nodes"], tablefmt="pretty",  showindex=range(1, len(runners_data) + 1), colalign=("left", "left", "right"))
+            [[r.runner_domain, f'{r.total_nodes:,}'] for r in runners_data], ["domain", "# nodes"], colalign=("left", "right"), maxcolwidths=[22, 12])
         embed_n = DiscordEmbed(
             title=f"# nodes", description=f'```{description}```', color='ff66ff')
         self.webhook.add_embed(embed_n)
+        # self.webhook.content = f'```{description}```'
         self.webhook.execute()
-        time.sleep(2)
+        # time.sleep(2)
         self.webhook.remove_embeds()
 
         # Total tokens
         runners_data.sort(key=lambda r: (
             r.tokens, r.runner_domain_sort), reverse=True)
         description = tabulate(
-            [[r.runner_domain, f'{round(r.tokens):,}'] for r in runners_data], ["domain", "# tokens"], tablefmt="pretty",  showindex=range(1, len(runners_data) + 1), colalign=("left", "left", "right"))
+            [[r.runner_domain, f'{round(r.tokens):,}'] for r in runners_data], ["domain", "# tokens"], colalign=("left", "right"), maxcolwidths=[22, 12])
         embed_n = DiscordEmbed(
             title=f"# tokens", description=f'```{description}```', color='6a0dad')
         self.webhook.add_embed(embed_n)
+        # self.webhook.content = f'```{description}```'
         self.webhook.execute()
-        time.sleep(2)
+        # time.sleep(2)
         self.webhook.remove_embeds()
 
         # add csv dump if exists
@@ -107,6 +113,6 @@ class DiscordBot():
                     file=f.read(), filename='node_runners.csv')
 
         self.webhook.execute()
-        time.sleep(2)
+        # time.sleep(2)
 
         # logging.info(response)
