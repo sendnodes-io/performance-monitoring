95722a04142a06246a5aa5a81794290ff51a05d4
BigBoss <95193764+0xBigBoss@users.noreply.github.com>
2022-11-05 12:57:30 -0700

    add api (#4)

---
+++ app.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+import csv
+from main import get_stats, post_discord_message
+from flask import Flask, g, jsonify
+from flask_apscheduler import APScheduler
+import logging
+
+load_dotenv()
+
+# Initialise logger
+logging.basicConfig(level=logging.INFO,
+                    format='%(name)s - %(levelname)s - %(message)s')
+
+
+class Config:
+    SCHEDULER_API_ENABLED = False
+
+
+# create app
+app = Flask(__name__)
+app.config.from_object(Config())
+
+# initialize scheduler
+scheduler = APScheduler()
+
+scheduler.init_app(app)
+
+netperf_data = None
+runners_perf_data = None
+
+
+def get_runners_perf_data():
+    global netperf_data
+    global runners_perf_data
+    netperf_data, runners_perf_data = calc_runners_perf_data()
+    return runners_perf_data
+
+
+def calc_runners_perf_data():
+    return get_stats(
+        int(os.environ.get('APP_TOP_NODE_RUNNERS', 25))
+    )
+
+
+@scheduler.task('cron', id='do_post_runners_perf_data', hour='*')
+def post_runners_perf_data():
+    global netperf_data
+    global runners_perf_data
+    netperf_data, runners_perf_data = calc_runners_perf_data()
+
+    # generate csv file
+    keys = list(
+        {k: v for k, v in runners_perf_data[0].dict().items() if v is not None}.keys())
+    with open('node_runners.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(
+            [{k: v for k, v in rp.dict().items() if v is not None}
+             for rp in runners_perf_data]
+        )
+
+    print([netperf_data])
+    print(runners_perf_data)
+
+    post_discord_message(netperf_data, runners_perf_data)
+
+    return runners_perf_data
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+
+@app.route('/v1/runners-perf')
+def runners_perf():
+    global netperf_data
+    global runners_perf_data
+    if runners_perf_data is None:
+
+        for job in scheduler.get_jobs():
+            job.modify(next_run_time=datetime.now())
+
+        return jsonify({'error': 'No data available'}), 404
+    else:
+        return jsonify([{k: v for k, v in rp.dict().items() if v is not None} for rp in runners_perf_data])
+
+
+scheduler.start()
+
+if __name__ == "__main__":
+    app.run()
--- readme.md
+++ readme.md
@@ -3,15 +3,20 @@ This repo is generating performance reports for the nodes runners based on pokt-
 Sometimes the API doesn't return data for some of the nodes runners - although we attempt to retry on connection failure we won't return data for the node runners which we have failed to fetch from the API.
 
 ## Overall flow description
+
 1. We get the list of the top runners from pokt-scan API
 2. For each top runners we fetch statistics (avg perf 6 / 24 / 48 hrs)
 3. We post to SendNodes Discord channel the statistics
 4. We post to twitter the performances
 
 ## Usage
+
 ### Environment variables
-----
+
+---
+
 In order to operate successfully the bot rely on a few environment variables
+
 - **GRAPHQL_URL**: https://www.poktscan.com/api/graphql,
 - **GRAPHQL_API_KEY_1**: KEY FOR GRAPHQL API
 - **GRAPHQL_API_KEY_2**: ALT KEY FOR GRAPHQL API
@@ -20,12 +25,20 @@ In order to operate successfully the bot rely on a few environment variables
 - **AWS_S3_BUCKET**:BUCKET NAME
 - **AWS_REGION**: AWS S3 REGION
 - **DISCORD_HOOK**: HOOK FOR WRITTING TO DISCORD
-- **TWITTER_API_KEY**: TWITTER APP ID 
+- **TWITTER_API_KEY**: TWITTER APP ID
 - **TWITTER_API_SECRET**: TWITTER APP SECRET
-- **TWITTER_ACCESS_TOKEN**: TOKEN FOR WRITTING 
+- **TWITTER_ACCESS_TOKEN**: TOKEN FOR WRITTING
 - **TWITTER_ACCESS_TOKEN_SECRET**: TOKEN SECRET FOR WRITTING
+
 ### Run the program
-----
-In order to run the program you can specificy command line arguments: 
-- -d will get the statistics and post the results to Discord 
-- -t will get the statistics and psot the results to Twitter 
+
+---
+
+In order to run the program you can specificy command line arguments:
+
+- -d will get the statistics and post the results to Discord
+- -t will get the statistics and psot the results to Twitter
+
+```
+python ./main.py -d -n 25
+```
